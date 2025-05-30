aef6aa56d0c04a346c9b4687a11445d1ffcdecf0
Rodrigo Garcia <dev.raxielh@gmail.com>
2025-05-29 20:58:54 -0500

    init

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,4 +1,3 @@
-import PageMeta from "../../components/common/PageMeta";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import MapaComunasAsistencias from "../MapaComunasAsistencias";
