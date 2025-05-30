4ff1dda607d79c944c41203bc36a77d49f9c1175
Rodrigo Garcia <dev.raxielh@gmail.com>
2025-05-29 21:12:09 -0500

    init

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import MapaComunasAsistencias from "../MapaComunasAsistencias";
+import { useNavigate } from "react-router-dom";
 
 interface Asistencia {
   comuna_actividad: string;
@@ -8,11 +9,17 @@ interface Asistencia {
 }
 
 export default function Dashboard() {
+  const navigate = useNavigate();
   const [datosFiltrados, setDatosFiltrados] = useState<Asistencia[]>([]);
 
   useEffect(() => {
     const token = localStorage.getItem("access");
-    if (!token) return;
+
+    if (!token) {
+      navigate("/");
+      return;
+    }
+
 
     axios
       .get<Asistencia[]>(import.meta.env.VITE_API_URL + "asistencias", {
